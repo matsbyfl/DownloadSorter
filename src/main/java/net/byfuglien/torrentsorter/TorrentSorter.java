aef6345813b32382e9a7f38a419c1af03221df8f
Mats Byfuglien <mats@byfuglien.net>
2013-04-28 01:50:40 +0200

    Pseudocode

--- src/main/java/net/byfuglien/torrentsorter/TorrentSorter.java
+++ src/main/java/net/byfuglien/torrentsorter/TorrentSorter.java
@@ -8,4 +8,20 @@ package net.byfuglien.torrentsorter;
  * To change this template use File | Settings | File Templates.
  */
 public class TorrentSorter {
+    public static void main(String[] args) {
+        /*
+        * Src og dest som inputparametre
+        * Iterere over src mappen
+         * lage en liste shows alt som ligger i default gjøres det ikke noe med
+         * hvis tror det er tv show, legg til i shows lista, kanskje også lese basert på regexp liste som sendes inn?
+            * rekursivt. Hvis mappe, ser mappenavnet ut som et serienavn?
+            * har imdb eller andre en resttjeneste man kan spørre?
+            *
+
+        *  itere over listene og plassere i mapper i target shows
+        *  Kanskje også gjøre det for musikk
+        *  Sende mail til meg selv om status hver gang jobben er kjørt og noe er flyttet
+        *
+        * */
+    }
 }
